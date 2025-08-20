b2d772cdf1872c789e6821f03f606ab36dd673d6
Nathan-ma <than.matos@gmail.com>
2025-08-20 03:22:37 -0300

    project: Fix compiler not using local FreeRTOSConfig.h
    Signed-off-by: Nathan-ma <than.matos@gmail.com>

--- test/mocks/freertos/FreeRTOSConfig.h
+++ test/mocks/freertos/FreeRTOSConfig.h
@@ -18,7 +18,7 @@
 #define configUSE_TICK_HOOK						1
 #define configTICK_RATE_HZ				( ( portTickType ) 1000 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned portSHORT ) 64 ) /* This can be made smaller if required. */
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 64 * 1000000000 ) )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 64 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 16 )
 #define configUSE_TRACE_FACILITY    	0
 #define configUSE_16_BIT_TICKS      	0
